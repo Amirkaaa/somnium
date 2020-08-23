44c47d3cfe98fab359440b36b19fe19ad959e5b5
Amirkaaa <amirshabanov97@gmail.com>
2020-08-23 15:39:15 +0600

    Group of people can be in subordination of other user

--- group/models.py
+++ group/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 
@@ -7,6 +8,8 @@ class Group(models.Model):
     name = models.CharField(_('Название'), max_length=64)
     director = models.ForeignKey('Group', verbose_name=_('Начальство'), null=True, blank=True, on_delete=models.CASCADE,
                                  help_text=_('Выше стоящее руководство'), related_name='branches')
+    user = models.ForeignKey(get_user_model(), verbose_name=_('Руководитель'), null=True, blank=True, on_delete=models.CASCADE,
+                             help_text=_('В подчинении у одного из руководителей'), related_name='subordination')
 
     def __str__(self):
         return '{} - {}'.format(self.name, self.director)
